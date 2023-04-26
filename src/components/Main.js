768cff823c2f5a4c4ee8806b70fc1b20b5ad1f04
dhanush29 <dhanush.maddula@gmail.com>
2023-04-26 19:02:12 +0530

    Added support for zoom meet

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,25 @@ import Editor from './Editor.js'
 import '../static/Main.css'
 
 function Main(){
-
+  const connectVideo = (e) => {
+    var url = e.target.value;
+    if (url != undefined || url != '') {
+        var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
+        var match = url.match(regExp);
+        if (match && match[2].length == 11) {
+            // Do anything for being valid
+            // if need to change the url to embed url then use below line
+            setNameState({name:url,value: 'https://www.youtube.com/embed/' + match[2] + '?autoplay=0'})
+        }
+        else {
+          setNameState({name:url,value:url})
+        }
+        
+    }
+  }
     const [nameState,setNameState] = useState({
-      name: "5qap5aO4i9A"
+      name: "https://www.youtube.com/watch?v=5qap5aO4i9A",
+      value: "https://youtube.com/embed/5qap5aO4i9A"
     })
     
     const opts = {
@@ -22,11 +38,19 @@ function Main(){
     <div className="column">
         <div class ="px-2 py-2">
         <form class="form-inline">
-            <label style= {{paddingInlineEnd:'15px'}}>Enter Video ID:</label>
-            <input class="form-control w-50 pl-2 rounded" type="text" value={nameState.name} onChange={e => {setNameState({name:e.target.value})}} />
+            <label style= {{paddingInlineEnd:'15px'}}>Enter Video/Meeting URL:</label>
+            <input class="form-control w-50 pl-2 rounded" type="text" value={nameState.name} onChange={e => {connectVideo(e)}} />
         </form>
         </div>
-        <YouTube className= "video" videoId={nameState.name} opts={opts}/>
+        <iframe src={nameState.value}
+        className="video"
+        frameborder='0'
+        allow='autoplay; encrypted-medial; gyroscope; picture-in-picture'
+        allowfullscreen = "1"
+        title='video'
+        width = "97%"
+        frameborder="0"
+        />
     </div>
     <div className="column" >
         <main style={{maxHeight:'70vh'}}>
